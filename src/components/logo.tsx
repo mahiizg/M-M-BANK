393df68d94291fbda8205bba3e547c0d0a7d0ff2
Mahi Gupta <guptamahi0207@gmail.com>
2025-10-14 19:00:59 +0000

    and contrast bank name so that it is visible

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={cn("flex items-center gap-2", className)}>
       <Landmark className="h-8 w-8 text-primary" />
-      <span className="text-2xl font-bold tracking-tighter font-headline text-primary">
+      <span className="text-2xl font-bold tracking-tighter text-primary group-data-[sidebar=sidebar]:text-sidebar-foreground font-headline">
         M&M Bank
       </span>
     </div>
