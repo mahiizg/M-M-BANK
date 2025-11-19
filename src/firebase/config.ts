d8389e379ca851c183cf2c92531cb92adad94594
Mahi Gupta <guptamahi0207@gmail.com>
2025-11-19 10:15:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8566423112-70988",
+  "appId": "1:474065079502:web:aac0e591d1471b8500149f",
+  "apiKey": "AIzaSyCSqlUq7a_Mg0grPPbBh7Zt4QupOe9DRKA",
+  "authDomain": "studio-8566423112-70988.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "474065079502"
+};
